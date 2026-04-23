b32c61aebaebcab0083751670df49909ff2cac09
xDk <linuxkali717@gmail.com>
2026-04-23 16:24:45 +0300

    fixed ui

--- src/components/EmptyChat.tsx
+++ src/components/EmptyChat.tsx
@@ -33,7 +33,7 @@ export default function EmptyChat() {
 
   return (
     <div className="flex flex-col gap-10 w-full h-full items-center justify-center">
-    <div className="flex flex-col gap-10 max-lg:-translate-y-20 items-center justify-center pb-20">
+    <div className="flex flex-col gap-10 items-center justify-center pb-20">
       <div className="p-8 lg:text-5xl text-3xl text-center flex justify-center">{currentGreeting}</div>
 
       <div className="max-w-3/5 gap-3 flex max-md:hidden justify-center flex-wrap">
