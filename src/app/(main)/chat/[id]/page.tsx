3af4028355bc3a6f756b2c0ba8a4977fd6fbc9b1
x <x>
2026-04-23 06:47:21 +0300

    fixing session management

--- src/app/(main)/chat/[id]/page.tsx
+++ src/app/(main)/chat/[id]/page.tsx
@@ -5,34 +5,34 @@ import { useParams, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
 import { FaCircleNotch } from "react-icons/fa";
-// import { Message } from "ai";
+import { Message } from "ai";
 
 import ChatUI from "@/components/ChatUI";
 import InputBar from "@/components/InputBar";
-// import { Session } from "@/lib/types";
-import { getSession, addMessagestoLocalSession } from "@/lib/utils";
+import { Session } from "@/lib/types";
+import { getSession, updateSession, addMessagestoLocalSession } from "@/lib/utils";
 
 export default function ChatEach() {
   const { id } = useParams();
   const params = useSearchParams();
-  // const [chatList, setChatList] = useState<Message[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [initChat, setInitChat] = useState(false)
+  const [initialMessages, setInitialMessages] = useState<Message[]>([])
   const { input, setInput, handleSubmit, handleInputChange, messages, isLoading } =
-    useChat();
+    useChat({ initialMessages });
 
-  const feedContextToModel = async () => {
-    setLoading(true);
-    try {
-      // TODO: Handle feeding the context to the model through api call
-      // BUG: Assuming that giving context to the model will fill 'messages' state with the previous messages
-    } catch (err) {
-      setError(`Error: ${err}`);
-    } finally {
-      setLoading(false);
-    }
-  };
+  // const feedContextToModel = async (prevChat: Message[]) => {
+  //   setLoading(true);
+  //   try {
+  //     // TODO: Handle feeding the context to the model through api call
+  //     // BUG: Assuming that giving context to the model will fill 'messages' state with the previous messages
+  //   } catch (err) {
+  //     setError(`Error: ${err}`);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
   useEffect(() => {
     const initChat = params.get("init-chat")
@@ -43,8 +43,12 @@ export default function ChatEach() {
   }, [])
 
   useEffect(() => {
-    addMessagestoLocalSession(id as string, [messages[-1]]);
-    // console.log(messages);
+    const idStr = id?.toString();
+    if (!idStr) redirect(`/chat`);
+
+    const currentSession = getSession(idStr);
+    currentSession.messages = messages
+    updateSession(currentSession)
   }, [messages, id]);
 
   useEffect(() => {
@@ -56,9 +60,8 @@ export default function ChatEach() {
 
     if (!currentChat.length) return setLoading(false);
 
-    // TODO: Figure out the param
-    feedContextToModel();
-    return;
+    setInitialMessages(currentChat);
+    return setLoading(false);
   }, [id]);
 
   return (
