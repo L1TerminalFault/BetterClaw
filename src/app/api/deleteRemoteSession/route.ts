2b2845e77599c8c17714edc22441f5c7892bce0c
x <x>
2026-04-28 03:22:17 +0300

    update on the deleteSession route

--- src/app/api/deleteRemoteSession/route.ts
+++ src/app/api/deleteRemoteSession/route.ts
@@ -1,9 +1,9 @@
-import { Session, dbConnect } from "@/db/model";
+import { dbConnect, deleteSession } from "@/db/model";
 
 export const GET = async (req: Request) => {
-  await dbConnect()
+  await dbConnect();
 
   const id = req.url.split("?id=")[1];
 
-  await Session.findOneAndDelete({ id });
+  await deleteSession(id);
 };
